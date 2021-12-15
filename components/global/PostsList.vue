7b21b6a35a2b7c374e3daa1a85a11071a88925d4
dominickjay217 <dominickjay217@gmail.com>
2021-12-15 12:45:23 +0000

    Add external posts markdown file and output to template

--- components/global/PostsList.vue
+++ components/global/PostsList.vue
@@ -48,57 +48,11 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.post {
-  font-size: var(--step-0);
-  display: flex;
-  gap: var(--grid-gap);
-  justify-content: space-between;
-  align-items: flex-start;
-  position: relative;
-  padding: 10px;
-  padding-right: 40px;
-  text-align: left;
-  overflow: hidden;
-  &-list {
-    padding: 0;
-    margin: 0;
-    list-style: none;
-  }
-  & .title {
-    font-weight: var(--fw-base-m);
-    flex: 1;
-    font-size: var(--step-0);
-    margin-bottom: 10px;
-    position: relative;
-  }
-  & a.title {
-    text-decoration: underline;
-    text-decoration-color: var(--clr-links);
-    text-decoration-thickness: 2px;
-    transition: .2s ease-out text-decoration-color;
-    &:hover {
-      text-decoration-color: var(--clr-links-active);
-    }
-  }
-}
 
-time {
-  text-align: right;
-  display: flex;
-  font-family: var(--ff-alt);
-  font-size: var(--step-0);
-  font-weight: var(--fw-base-lg);
-  opacity: 0.75;
-}
-
-.status {
-    font-weight: var(--fw-base);
-}
-
-@media (max-width: 640px) {
-  .post {
-    padding: 10px 0;
-  }
+.post-list {
+  padding: 0;
+  margin: 0;
+  list-style: none;
 }
 
 </style>
