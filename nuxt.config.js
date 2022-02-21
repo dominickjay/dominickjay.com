5aacc54da2db36700853bd97b8e1d0670799dbc5
dominickjay217 <dominickjay217@gmail.com>
2022-02-21 12:48:12 +0000

    Add meta tags and analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  generate: {
+    fallback: true,
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -19,10 +22,65 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Dominick Jay - Creative Front End Developer',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Dominick Jay is an experienced creative Front-End Developer from Plymouth, UK, that specializes in fun, creative solutions.',
+      },
+      { hid: 'og:url', property: 'og:url', content: 'https://dominickjay.com' },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://dominickjay.com/images/card-image.png',
+      },
+      {
+        hid: 'twitter:title',
+        property: 'twitter:title',
+        content: 'Dominick Jay - Creative Front End Developer',
+      },
+      {
+        hid: 'twitter:description',
+        property: 'twitter:description',
+        content:
+          'Dominick Jay is an experienced creative Front-End Developer from Plymouth, UK, that specializes in fun, creative solutions.',
+      },
+      {
+        hid: 'twitter:image',
+        property: 'twitter:image',
+        content: 'https://dominickjay.com/images/card-image.png',
+      },
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        hid: 'twitter:url',
+        property: 'twitter:url',
+        content: 'https://dominickjay.com',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        hid: 'thesemetrics',
+        src: 'https://unpkg.com/thesemetrics@latest',
+        async: true,
+        type: 'text/javascript',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
