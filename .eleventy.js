ebec825d82652e8da5c716b48099eec98faade89
Dominick Jay <dominickjay217@gmail.com>
2022-11-25 13:15:48 +0000

    Remove eleventy-load

--- .eleventy.js
+++ .eleventy.js
@@ -99,30 +99,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(syntaxHighlight)
 
-  eleventyConfig.addPlugin(require('eleventy-load'), {
-    rules: [
-      {
-        test: /\.(md|html)$/,
-        loaders: [
-          {
-            loader: require('eleventy-load-html'),
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        loaders: [
-          {
-            loader: require('eleventy-load-js'),
-          },
-          {
-            loader: require('eleventy-load-file'),
-          },
-        ],
-      },
-    ],
-  })
-
   return {
     passthroughFileCopy: true,
   }
