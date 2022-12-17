fb54cda25df8e14ee2e93e932a0e6a5ee83a88c4
Dominick Jay <dominickjay217@gmail.com>
2022-12-17 21:07:05 +0000

    remove env variables

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -15,7 +15,7 @@ export default async (request, context) => {
     });
 
     const response = await fetch(
-      `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=zerosandones217&limit=10&api_key=86a5b41a85035739e32c576f027c4765&format=json`
+      `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=zerosandones217&limit=10&api_key=process.env.LASTFM_API&format=json`
     )
 
     const data = await response.json();
