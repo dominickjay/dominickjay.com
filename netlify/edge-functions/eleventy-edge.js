4450fb964bc9d68e1e1547bd121d9cf2e53528b7
Dominick Jay <dominickjay217@gmail.com>
2023-01-24 20:43:57 +0000

    Update eleventy-edge.js

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -15,13 +15,10 @@ export default async (request, context) => {
     });
 
     const response = await fetch(
-      "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=zerosandones217&limit=10&api_key=" + process.env.LASTFM_API + "&format=json"
+      "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=zerosandones217&limit=10&api_key=86a5b41a85035739e32c576f027c4765&format=json"
     )
 
     const data = await response.json();
-
-    // console.log(data.recenttracks.track[0].name)
-    // console.log(data.recenttracks.track[0].artist['#text'])
 
     edge.config((eleventyConfig) => {
       // Add some custom Edge-specific configuration
