b9bbb53c0c1b84d2b8c34f059debb19da285fe49
Dominick Jay <dominickjay217@gmail.com>
2023-11-21 21:08:42 +0000

    add scheduled deployments

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch'
+import { schedule } from '@netlify/functions'
+
+// This is a sample build hook URL
+const BUILD_HOOK =
+  'https://api.netlify.com/build_hooks/655d1b4b6ef2c71047d3ba28'
+
+// Schedules the handler function to run at midnight on
+// Mondays, Wednesday, and Friday
+const handler = schedule('0 12,5 * * *', async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then(response => {
+    console.log('Build hook response:', response)
+  })
+
+  return {
+    statusCode: 200
+  }
+})
+
+export { handler }
