c15767341400b620c6fa5d0b5f4ade378e5e71ca
Dominick Jay <dominickjay217@gmail.com>
2024-11-13 08:49:25 +0000

    change auto deploy

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -5,15 +5,15 @@ import { schedule } from '@netlify/functions'
 const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/655d1b4b6ef2c71047d3ba28'
 
-const handler = schedule('0 9,12,17 * * *', async () => {
+const handler = schedule('0 9,17 * * *', async () => {
   await fetch(BUILD_HOOK, {
-    method: 'POST'
-  }).then(response => {
+    method: 'POST',
+  }).then((response) => {
     console.log('Build hook response:', response)
   })
 
   return {
-    statusCode: 200
+    statusCode: 200,
   }
 })
 
