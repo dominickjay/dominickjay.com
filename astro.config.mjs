2fc72264b9fd136cfb5948a5ba4841f4eaaebce9
Dominick Jay <dominickjay217@gmail.com>
2025-01-23 15:45:16 +0000

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import alpinejs from '@astrojs/alpinejs';
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dominickjay.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), alpinejs()],
 });
