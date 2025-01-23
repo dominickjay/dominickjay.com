7c3d6ada3f92b0093114d5ca644e12d5a3d100be
Dominick Jay <dominickjay217@gmail.com>
2025-01-23 06:59:55 +0000

    start of astro work

--- config/shortcodes/index.js
+++
@@ -1,7 +0,0 @@
-const imageShortcodePlaceholder = require('./imagePlaceholder');
-const shareImageShortcode = require('./shareImageShortcode');
-
-module.exports = {
-  shareImageShortcode,
-  imageShortcodePlaceholder
-};
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dominick Jay';
+export const SITE_DESCRIPTION = 'The personal site and portfolio of Plymouth-based front-end web developer Dominick Jay';
+export const SITE_TAGLINE = 'Leave it better than you found it';
