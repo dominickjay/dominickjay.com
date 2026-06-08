43185e6cbc4e94580a5f22ac02551d2013702283
Dominick Jay <dominickjay217@gmail.com>
2026-06-08 11:05:07 +0100

    fix publication

--- src/pages/.well-known/site.standard.publication.ts
+++ src/pages/.well-known/site.standard.publication.ts
@@ -5,8 +5,8 @@ import { generatePublicationWellKnown } from "@bryanguffey/astro-standard-site";
 export const GET: APIRoute = () => {
   return new Response(
     generatePublicationWellKnown({
-      did: "did:plc:wbe7yhdrp57bolsjx4xf7mr6/site.standard.publication/3mnnehdgj3kxu", // Your DID
-      publicationRkey: "3mnnehdgj3kxu", // From create-publication output
+      did: "did:plc:wbe7yhdrp57bolsjx4xf7mr6",
+      publicationRkey: "3mnnehdgj3kxu",
     }),
     { headers: { "Content-Type": "text/plain" } },
   );
