a904764ca308d5d3d94463fd82bfd1eb09bfde63
Dominick Jay <dominickjay217@gmail.com>
2026-06-10 07:14:03 +0100

    remove drafts from rss

--- src/pages/post-feed.xml.js
+++ src/pages/post-feed.xml.js
@@ -3,32 +3,28 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-	const blogPosts = await getCollection("writing");
+  const blogPosts = await getCollection("writing", ({ data }) => !data?.draft);
 
-	const items = blogPosts.map((post) => {
-		const title = post.data.title ?? "Untitled";
-		const description = post.data.description ?? "";
-		const rawDate = post.data.pubDate;
-		const pubDate =
-			rawDate instanceof Date
-				? rawDate
-				: new Date(rawDate);
-		const validDate =
-			!Number.isNaN(pubDate.getTime()) ? pubDate : new Date();
+  const items = blogPosts.map((post) => {
+    const title = post.data.title ?? "Untitled";
+    const description = post.data.description ?? "";
+    const rawDate = post.data.pubDate;
+    const pubDate = rawDate instanceof Date ? rawDate : new Date(rawDate);
+    const validDate = !Number.isNaN(pubDate.getTime()) ? pubDate : new Date();
 
-		return {
-			title,
-			description,
-			pubDate: validDate,
-			link: `/writing/${post.id}/`,
-		};
-	});
+    return {
+      title,
+      description,
+      pubDate: validDate,
+      link: `/writing/${post.id}/`,
+    };
+  });
 
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items,
-		customData: `<language>en-us</language>`,
-	});
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items,
+    customData: `<language>en-us</language>`,
+  });
 }
